f9c231e1565dedd36e94adbbe2c6ad1f6b314fe5
Regis Nde Tene <chopinregis@users.noreply.github.com>
2024-05-22 19:08:58 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,72 +1,64 @@
-variable "prefix" {
-  default = "tfvmex"
+resource "azurerm_resource_group" "vm_rg" {
+  name     = var.resource_group_name
+  location = var.location
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "${var.prefix}-resources"
-  location = "West Europe"
-}
+resource "azurerm_virtual_machine" "myvms" {
+  count                = length(var.vm_names)
+  name                 = var.vm_names[count.index]
+  location             = azurerm_resource_group.vm_rg.location
+  resource_group_name  = azurerm_resource_group.vm_rg.name
+  network_interface_ids = [azurerm_network_interface.my_nic[count.index].id]
+  vm_size              = var.vm_size
 
-resource "azurerm_virtual_network" "main" {
-  name                = "${var.prefix}-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-}
-
-resource "azurerm_subnet" "internal" {
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
-
-resource "azurerm_network_interface" "main" {
-  name                = "${var.prefix}-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-
-  ip_configuration {
-    name                          = "testconfiguration1"
-    subnet_id                     = azurerm_subnet.internal.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-resource "azurerm_virtual_machine" "main" {
-  name                  = "${var.prefix}-vm"
-  location              = azurerm_resource_group.example.location
-  resource_group_name   = azurerm_resource_group.example.name
-  network_interface_ids = [azurerm_network_interface.main.id]
-  vm_size               = "Standard_DS1_v2"
-
-  # Uncomment this line to delete the OS disk automatically when deleting the VM
-  # delete_os_disk_on_termination = true
-
-  # Uncomment this line to delete the data disks automatically when deleting the VM
-  # delete_data_disks_on_termination = true
-
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
-  }
   storage_os_disk {
-    name              = "myosdisk1"
+    name              = "myosdisk${count.index + 1}"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
   os_profile {
-    computer_name  = "hostname"
-    admin_username = "testadmin"
+    computer_name  = var.vm_names[count.index]
+    admin_username = "adminuser"
     admin_password = "Password1234!"
   }
+
   os_profile_linux_config {
     disable_password_authentication = false
   }
-  tags = {
-    environment = "staging"
+}
+
+resource "azurerm_network_interface" "my_nic" {
+  count               = length(var.vm_names)
+  name                = "myNIC${count.index + 1}"
+  location            = azurerm_resource_group.vm_rg.location
+  resource_group_name = azurerm_resource_group.vm_rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.my_subnet.id
+    private_ip_address_allocation = "Dynamic"
   }
+}
+
+resource "azurerm_virtual_network" "my_vnet" {
+  name                = "myVNet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.vm_rg.location
+  resource_group_name = azurerm_resource_group.vm_rg.name
+}
+
+resource "azurerm_subnet" "my_subnet" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.vm_rg.name
+  virtual_network_name = azurerm_virtual_network.my_vnet.name
+  address_prefix       = "10.0.1.0/24"
 }
