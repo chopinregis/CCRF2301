f6ae9459a11cd5d098cc60fd30595607e81a04cc
Regis Nde Tene <chopinregis@users.noreply.github.com>
2024-05-22 19:59:16 -0400

    Update variables.tf

--- question12/variables.tf
+++ question12/variables.tf
@@ -56,3 +56,22 @@ variable "admin_password" {
   description = "password of the admin"
   type        = string
 }
+
+//
+//
+
+variable "vm_config" {
+  description = "YAML configuration for VMs"
+  type        = any
+  default     = {}
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region for the resource group"
+  type        = string
+}
