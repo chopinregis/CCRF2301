682e4cf667e26aac9b8d41d2ee1f942989605a3d
Regis Nde Tene <chopinregis@users.noreply.github.com>
2024-05-22 19:22:59 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -60,5 +60,5 @@ resource "azurerm_subnet" "my_subnet" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.vm_rg.name
   virtual_network_name = azurerm_virtual_network.my_vnet.name
-  address_prefix       = "10.0.1.0/24"
+  address_prefix       = ["10.0.2.0/24"]
 }
