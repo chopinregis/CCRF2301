361d9829c747b0712a39615bf79c65fa91df1e63
Regis Nde Tene <chopinregis@users.noreply.github.com>
2024-05-22 20:07:06 -0400

    Create locals.tf

---
+++ question12/locals.tf
@@ -0,0 +1,10 @@
+locals {
+
+  // Decode the YAML configuration file to get the VM settings
+  vm_config = yamldecode(file("${path.module}/configs/vm_config.yaml"))["vm_settings"]
+
+   common_tags = {
+      ManagedBy = "Terraform"
+      Environment = "Dev"
+   }
+}
